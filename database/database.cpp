27e04ee6d37617401b90b31a4e42fcb22249d141
Adriell8000B <adrielluiz73@gmail.com>
2025-09-12 23:47:47 -0300

    Update database methods

--- database/database.cpp
+++ database/database.cpp
@@ -1,26 +1,46 @@
 #include "database.hpp"
-#include <iostream>
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/builder/basic/kvp.hpp>
 #include <mongocxx/client.hpp>
+#include <mongocxx/collection-fwd.hpp>
+#include <mongocxx/database-fwd.hpp>
 #include <mongocxx/exception/exception.hpp>
+#include <iostream>
+#include <string>
 
-Database::Database(const std::string& uri):
-_uri(mongocxx::uri{uri}), _client(mongocxx::client{_uri}) {}
+Database::Database(
+	const std::string& uri,
+	const std::string& database_name,
+	const std::string& collection_name
+):
+_uri(mongocxx::uri{uri}),
+_client(mongocxx::client{_uri}),
+_database_name(database_name),
+_collection_name(collection_name) {}
 
 void Database::connect() {
 	try {
-		this->_client.database("MovieFlix").run_command(bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("ping", 1)));
+		this->_client.database(this->_database_name)
+		.run_command(
+			bsoncxx::builder::basic::make_document(
+				bsoncxx::builder::basic::kvp("ping", 1)
+			)
+		);
 		std::cout << "Mongodb connected!\n";
 	} catch(const mongocxx::exception& e) {
 		std::cout << "Couldn't connect to Mongodb: " << e.what() << "\n";
 	}
 }
 
-mongocxx::client& Database::getClient() {
-	return this->_client;
+mongocxx::database Database::getDatabase() {
+	return this->_client.database(this->_database_name);
+}
+
+mongocxx::collection Database::getCollection() {
+	return this->getDatabase().collection(this->_collection_name);
 }
 
 void Database::SetupDatabase() {
 	this->connect();
+	this->getCollection();
 }
