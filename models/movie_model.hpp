e11ea0192ca8646d654252e01e6dcfd48c94074a
Adriell8000B <adrielluiz73@gmail.com>
2025-09-12 23:46:49 -0300

    Fix: Name macros

--- models/movie_model.hpp
+++ models/movie_model.hpp
@@ -1,9 +1,9 @@
-#ifndef MOVIE_REPOSITORY_HPP_INCLUDED
-#define MOVIE_REPOSITORY_HPP_INCLUDED
+#ifndef MOVIE_MODEL_HPP_INCLUDED
+#define MOVIE_MODEL_HPP_INCLUDED
 
 #include <string>
 
-struct movies {
+struct Movies {
   std::string id;
   std::string movie_banner;
   std::string movie_rating;
