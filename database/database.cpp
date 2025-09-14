323b9e57005d19937ca6e345d32ea5426012a607
Adriell8000B <adrielluiz73@gmail.com>
2025-09-14 09:03:16 -0300

    Remove useless packages and attributes

--- database/database.cpp
+++ database/database.cpp
@@ -2,7 +2,6 @@
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/builder/basic/kvp.hpp>
 #include <mongocxx/client.hpp>
-#include <mongocxx/collection-fwd.hpp>
 #include <mongocxx/database-fwd.hpp>
 #include <mongocxx/exception/exception.hpp>
 #include <iostream>
@@ -10,17 +9,15 @@
 
 Database::Database(
 	const std::string& uri,
-	const std::string& database_name,
-	const std::string& collection_name
+	const std::string& database_name
 ):
 _uri(mongocxx::uri{uri}),
 _client(mongocxx::client{_uri}),
-_database_name(database_name),
-_collection_name(collection_name) {}
+_database_name(database_name) {}
 
 void Database::connect() {
 	try {
-		this->_client.database(this->_database_name)
+		this->getDatabase()
 		.run_command(
 			bsoncxx::builder::basic::make_document(
 				bsoncxx::builder::basic::kvp("ping", 1)
@@ -36,11 +33,6 @@ mongocxx::database Database::getDatabase() {
 	return this->_client.database(this->_database_name);
 }
 
-mongocxx::collection Database::getCollection() {
-	return this->getDatabase().collection(this->_collection_name);
-}
-
 void Database::SetupDatabase() {
 	this->connect();
-	this->getCollection();
 }
--- include/database.hpp
+++ include/database.hpp
@@ -1,7 +1,6 @@
 #ifndef DATABASE_HPP_INCLUDED
 #define DATABASE_HPP_INCLUDED
 
-#include <mongocxx/collection-fwd.hpp>
 #include <mongocxx/database-fwd.hpp>
 #include <string>
 
@@ -15,18 +14,15 @@ class Database {
 	mongocxx::uri _uri;
 	mongocxx::client _client;
 	const std::string& _database_name;
-	const std::string& _collection_name;
 
 	void connect();
 	mongocxx::database getDatabase();
-	mongocxx::collection getCollection();
 	
 	public:
 	void SetupDatabase();
 	explicit Database(
 		const std::string& uri,
-		const std::string& _database_name,
-		const std::string& _collection_name
+		const std::string& database_name
 	);
 };
 
