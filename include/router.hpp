e75311c4db3967005e01d5830f6e300f21fde04d
Adriell8000B <adrielluiz73@gmail.com>
2025-09-14 09:03:54 -0300

    Implement controller

--- include/router.hpp
+++ include/router.hpp
@@ -2,13 +2,18 @@
 #define ROUTER_HPP_INCLUDED
 
 #include "crow_all.h"
+#include "movie_controller.hpp"
 
 class Router {
 	private:
 		crow::App<crow::CORSHandler>& _app;
+		MovieController& _Movie_Controller;
 	
 	public :
-		Router(crow::App<crow::CORSHandler>& app);
+		Router(
+			crow::App<crow::CORSHandler>& app,
+			MovieController& Movie_Controller
+		);
 		void SetupRoutes();
 };
 
