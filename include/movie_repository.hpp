ca057e67c6af50476f5a142167635f1fe91ae395
Adriell8000B <adrielluiz73@gmail.com>
2025-09-14 09:03:41 -0300

    Added basic repository

--- include/movie_repository.hpp
+++ include/movie_repository.hpp
@@ -2,25 +2,29 @@
 #define MOVIE_REPOSITORY_HPP_INCLUDED
 
 #include "movie_model.hpp"
+#include <mongocxx/client.hpp>
 #include <mongocxx/collection-fwd.hpp>
-#include <mongocxx/database-fwd.hpp>
+#include <mongocxx/uri.hpp>
 #include <string>
 #include <vector>
 
 class MovieRepository {
 	private:
 	const Movies& _movie;
-	const mongocxx::database& _database;
-	mongocxx::collection& _collection;
+	mongocxx::uri _uri;
+	mongocxx::client _client;
+	const std::string& _database_name;
+	const std::string& _collection_name;
 
 	public:
-	MovieRepository(
+	explicit MovieRepository(
 		const Movies& movie,
-		const mongocxx::database& database,
-		mongocxx::collection& collection
+		const std::string& uri,
+		const std::string& database_name,
+		const std::string& collection_name
 	);
 
-	std::vector<std::string> GetMovies();
+	std::vector<std::string> RetrieveMovies();
 };
 
 #endif
--- repositories/movie-repository.cpp
+++ repositories/movie-repository.cpp
@@ -1,25 +1,32 @@
 #include "movie_model.hpp"
 #include "movie_repository.hpp"
 #include <exception>
+#include <mongocxx/client.hpp>
 #include <mongocxx/collection-fwd.hpp>
-#include <mongocxx/database-fwd.hpp>
 #include <mongocxx/database.hpp>
 #include <bsoncxx/json.hpp>
+#include <mongocxx/uri.hpp>
 #include <string>
 #include <vector>
 
 MovieRepository::MovieRepository(
 	const Movies& movie,
-	const mongocxx::database& database,
-	mongocxx::collection& collection
-):_movie(movie),
-	_database(database),
-	_collection(collection)
+	const std::string& uri,
+	const std::string& database_name,
+	const std::string& collection_name
+):
+_uri(mongocxx::uri{uri}),
+_client(mongocxx::client{_uri}),
+_movie(movie),
+_database_name(database_name),
+_collection_name(collection_name)
 {}
 
-std::vector<std::string> MovieRepository::GetMovies() {
+std::vector<std::string> MovieRepository::RetrieveMovies() {
 	std::vector<std::string> movies_vector;
-	auto cursor = this->_collection.find({});
+	auto cursor = this->_client
+	.database(this->_database_name)
+	.collection(this->_collection_name).find({});
 
 	try {
 		for(auto&& doc : cursor) {
