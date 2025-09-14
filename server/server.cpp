c6428718fa710995ea087df0dfe5030593ff75f6
Adriell8000B <adrielluiz73@gmail.com>
2025-09-14 13:42:25 -0300

    Basic format

--- server/server.cpp
+++ server/server.cpp
@@ -5,7 +5,12 @@
 #include <exception>
 #include <iostream>
 
-Server::Server(crow::App<crow::CORSHandler>& app, Router& router, Database& database,const uint16_t PORT) :
+Server::Server(
+	crow::App<crow::CORSHandler>& app,
+	Router& router,
+	Database& database,
+	const uint16_t PORT
+):
 _app(app),
 _router(router),
 _database(database),
